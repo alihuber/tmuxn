497698a3a5eeefb7837ac3b8790c554f5122d62e
Alexander Huber <alih83@gmx.de>
2017-11-12 17:20:13 +0100

    added name and windows validation and "debug" option

--- tmuxn.js
+++ tmuxn.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const kexec         = require('kexec');
+const shell         = require("shelljs");
 const utils         = require('./lib/utils');
 const ScriptBuilder = require('./lib/script_builder').ScriptBuilder;
 
@@ -10,10 +11,11 @@ tmuxn
   .usage('[new/start] <project_name>')
   .option('-c, --create <project_name>', 'Create new project with name')
   .option('-s, --start <project_name>',  'Start project with name')
-  .option('-k, --kill <project_name>',  'Kill project with name')
+  .option('-k, --kill <project_name>',   'Kill project with name')
+  .option('-d, --debug <project_name>',  'Print shell commands of project with name')
   .parse(process.argv);
 
-if(!tmuxn.create && !tmuxn.start && !tmuxn.kill) {
+if(!tmuxn.create && !tmuxn.start && !tmuxn.kill && !tmuxn.debug) {
   console.log("Display possible commands with tmuxn --help");
   console.log("No option given, aborting...");
   process.exit(0);
@@ -23,9 +25,8 @@ try {
   if(tmuxn.start) {
     utils.checkHealth();
     let loadedData  = utils.parseYamlByName(tmuxn.start);
-    // TODO: check root, project_name, windows are set
+    _checkLoadedData(loadedData);
     let execString  = new ScriptBuilder(loadedData).buildScript();
-    console.log(execString);
     // kexec(execString);
   }
   if(tmuxn.create) {
@@ -35,6 +36,20 @@ try {
   if(tmuxn.kill) {
     // TODO implement kill template
   }
+  if(tmuxn.debug) {
+    utils.checkHealth();
+    let loadedData  = utils.parseYamlByName(tmuxn.debug);
+    _checkLoadedData(loadedData);
+    let execString  = new ScriptBuilder(loadedData).buildScript();
+    console.log(execString);
+  }
 } catch (e) {
   console.log(e);
+}
+
+function _checkLoadedData(loadedData) {
+  if(!loadedData.name || !loadedData.windows) {
+    shell.echo('The config file contains errors: no name or no windows set.');
+    shell.exit(1);
+  }
 }
